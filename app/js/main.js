8dc8e7656f1a0befb978ee86cd9fbccb64fb5900
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-03-06 01:00:16 +0100

    JS interactions and fixing responsive layout

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,12 @@ const searchBtn = document.body.querySelector('.search__bar__form--button');
 const artistQuery = document.body.querySelector('.search__bar__form--input');
 const container = document.body.querySelector('.container');
 const form = document.body.querySelector('#search_form');
-const error = document.body.querySelector('.error_message'); 
+const error = document.body.querySelector('.error_message');
 const parent = document.body.querySelector('.search__bar');
 const wrapper = document.body.querySelector('.search__bar__wrapper');
 const header = document.body.querySelector('.search__bar__wrapper--header');
 const formContainer = document.querySelector('.search__bar__form--container');
-const formIcon = document.querySelector('.search__bar__form--icon'); 
+const formIcon = document.querySelector('.search__bar__form--icon');
 const artist = document.querySelectorAll('.album__list__details--artist');
 const title = document.querySelectorAll('.album__list__details--title');
 
@@ -17,58 +17,58 @@ const title = document.querySelectorAll('.album__list__details--title');
 
 
 // Event Listeners
-searchBtn.addEventListener('click',getData,false); /* Show Data */
-form.addEventListener('submit',tabSearch,false);  /* Show Data when pressed TAB on search input field */
-artistQuery.addEventListener('keydown',hideError,false);
-window.addEventListener('scroll',fixedSearchBar,false);
+searchBtn.addEventListener('click', getData, false); /* Show Data */
+form.addEventListener('submit', tabSearch, false); /* Show Data when pressed TAB on search input field */
+artistQuery.addEventListener('keydown', hideError, false);
+window.addEventListener('scroll', fixedSearchBar, false);
 // Copy input value
 
-function searchArtist(){
+function searchArtist() {
     const artist = artistQuery.value;
-    const query = artist.replace(/\s/g,"+").toLowerCase(); /* replace spaces with + sign to provide correct content request */
+    const query = artist.replace(/\s/g, "+").toLowerCase(); /* replace spaces with + sign to provide correct content request */
     return query;
-   
+
 }
 
 // Get data from database
-function getData(){
-    container.innerHTML =''; /* Remove elements from previous search */
-    const data = searchArtist(); 
+function getData() {
+    container.innerHTML = ''; /* Remove elements from previous search */
+    const data = searchArtist();
     const method = 'GET';
-    const url = `https://itunes.apple.com/search?term=${data}&entity=album&attribute=artistTerm`;  /* Content request */
+    const url = `https://itunes.apple.com/search?term=${data}&entity=album&attribute=artistTerm`; /* Content request */
     const xhr = new XMLHttpRequest();
 
-    xhr.open(method,url,true);/* Open AJAX request */
-    xhr.onload = function(){
-        if(this.status == 200 && this.readyState == 4){
+    xhr.open(method, url, true); /* Open AJAX request */
+    xhr.onload = function () {
+        if (this.status == 200 && this.readyState == 4) {
             const dataReceived = parseData(this); /* Parse this.responseText */
-            const outputData = dataReceived.results; 
-            console.log(outputData);
-            if (outputData.length > 0 ){ /* If there are any results display them on page */
-              loadData(outputData);
-            }else{
+            const outputData = dataReceived.results;
+            if (outputData.length > 0) { /* If there are any results display them on page */
+                loadData(outputData);
+            } else {
                 error.style.display = 'block';
             }
         }
     }
     xhr.send();
-    artistQuery.value = '';/* Reset input field */
+    artistQuery.value = ''; /* Reset input field */
 }
 
 // Helper function to parse data from server
 
-function parseData(data){
+function parseData(data) {
     let response = JSON.parse(data.responseText);
     return response;
 }
 
 // Show data on website
-/* Create document fragment, fill it with correct information retrieved from the database and append each result to div with 'container' class. */
+/* Create document fragment, fill it with correct information retrieved from the database and append each result to div with 'container' class. 
+Truncate title or artist strings on smallest screens */
 
-function loadData(output){
+function loadData(output) {
 
-    for ( let i = 0; i < output.length; i++){
-    
+    for (let i = 0; i < output.length; i++) {
+
         let frag = document.createRange().createContextualFragment(`  
         <section class="album__list">
             <div class="album__list__album--item">
@@ -92,76 +92,160 @@ function loadData(output){
                 </div>
             </div>
         </section>`);
-       container.appendChild(frag);
+        container.appendChild(frag);
 
     }
     const artist = Array.from(document.querySelectorAll('.album__list__details--artist'));
     const title = Array.from(document.querySelectorAll('.album__list__details--title'));
-    
-    
-// media query event handler
-
-if (matchMedia) {
-  const mq = window.matchMedia("(max-width: 575.98px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-}
 
 
-// media query change
+    // media query event handler
+
+    if (matchMedia) {
+        const mq = window.matchMedia("(max-width: 575.98px)");
+        const lq = window.matchMedia("(min-width:1200px)");
+        const dq = window.matchMedia("(min-width: 992px) and (max-width: 1199.98px)");
+        mq.addListener(WidthChange);
+        lq.addListener(changeMarginXl);
+        dq.addListener(changeMarginL);
+        WidthChange(mq);
+        changeMarginXl(lq);
+        changeMarginL(dq);
+    }
+
+
+    // media query change
     function WidthChange(mq) {
         if (mq.matches) {
-        shave('.album__list__details--artist',100); /* Truncate strings in artist name and title in smallest resolution */
-        shave('.album__list__details--title',50);
-    } else{
-        shave('.album__list__details--artist',500);
-        shave('.album__list__details--title',500);
-    }
+            shave('.album__list__details--artist', 100); /*_ Truncate strings in artist name and title in smallest resolution */
+            shave('.album__list__details--title', 50);
+        } else {
+            shave('.album__list__details--artist', 500);
+            shave('.album__list__details--title', 500);
+        }
     }
 
+    //  Polish margins in fixed search bar.
+
+    function changeMarginXl(lq) {
+        if (lq.matches) {
+            header.style.marginTop = '0';
+            form.style.marginTop = '-3rem';
+            searchBtn.style.marginTop = '-2rem';
+        }
+    }
+
+    function changeMarginL(dq) {
+
+        if (dq.matches) {
+            form.style.marginTop = '-2rem';
+            searchBtn.style.marginTop = '-1rem';
+        }
+    }
+
+    // Animate first output data
+    window.sr = ScrollReveal();
+    sr.reveal('.album__list', {
+        delay: 300,
+        origin: 'bottom',
+        distance: '3rem',
+        easing: 'ease-in-out',
+        container: container,
+        mobile: false,
+        reset: true,
+    });
+
+
+
+    // Prevent jumping content when fixed search bar appears
+    function preventJump() {
+        if (parent.classList.contains('search__bar--fixed')) {
+            const height2 = getElemHeight(parent);
+            const diff = height - height2 + 'px'; /* Set margin-top value for container element to prevent jump */
+            container.style.marginTop = diff;
+        } else {
+            container.style.marginTop = '0';
+        }
+
+    }
+    // Helper function to determinate element clientHeight 
+    function getElemHeight(el){
+        return el.clientHeight;
+    }
+    //  variable in outer scope to save element height value before fixed search bar pops up.
+    const height = getElemHeight(wrapper);
+
+    //  Event Listener
+    window.addEventListener('scroll', preventJump, false);
 }
- 
-
 
 //  Show data when user presses ENTER key.
-function tabSearch(e){
+function tabSearch(e) {
     e.preventDefault();
-    if (e.target.tagName == 'FORM'){
+    if (e.target.tagName == 'FORM') {
         searchBtn.click();
     }
 }
 
-function hideError(){
-    if(error.style.display = 'block'){
+function hideError() {
+    if (error.style.display = 'block') {
         error.style.display = 'none';
     }
 }
 
-function fixedSearchBar(){
+// Show fixed Search Bar
+function fixedSearchBar() {
     const distance = calculateScrollHeight();
-    if( distance > 10){
-    //     console.log('10%scroll')
+    if (distance > 10) {
+        //     console.log('10%scroll')
 
-        parent.className = 'search__bar--fixed';
-        wrapper.className= 'search__bar__wrapper--fixed';
-        header.className= 'search__bar__wrapper--header--fixed';
-        form.className= 'search__bar__form--fixed';
-        formContainer.className='search__bar__form--container--fixed';
-        formIcon.className= 'search__bar__form--icon--fixed';
-        artistQuery.className='search__bar__form--input--fixed';
-        searchBtn.className='search__bar__form--button--fixed';
-        } else if(distance <= 10){
-        parent.className = 'search__bar';
-        wrapper.className= 'search__bar__wrapper';
-        header.className= 'search__bar__wrapper--header';
-        form.className= 'search__bar__form';
-        formContainer.className='search__bar__form--container';
-        formIcon.className= 'search__bar__form--icon';
-        artistQuery.className='search__bar__form--input';
-        searchBtn.className='search__bar__form--button';
+        parent.className = 'search__bar--fixed ';
+        wrapper.className = 'search__bar__wrapper--fixed';
+        header.className = 'search__bar__wrapper--header--fixed';
+        form.className = 'search__bar__form--fixed';
+        formContainer.className = 'search__bar__form--container--fixed';
+        formIcon.className = 'search__bar__form--icon--fixed';
+        artistQuery.className = 'search__bar__form--input--fixed';
+        searchBtn.className = 'search__bar__form--button--fixed';
+
+        if (matchMedia) {
+            const lq = window.matchMedia("(min-width:1200px)");
+            const dq = window.matchMedia("(min-width: 992px) and (max-width: 1199.98px)");
+            lq.addListener(changeMarginXl);
+            dq.addListener(changeMarginL);
+            changeMarginXl(lq);
+            changeMarginL(dq);
         }
-        
+
+        function changeMarginXl(lq) {
+            if (lq.matches) {
+                document.body.querySelector('.search__bar__wrapper--header--fixed').style.marginTop = '1rem';
+                document.body.querySelector('.search__bar__form--fixed').style.marginTop = '0';
+                document.body.querySelector('.search__bar__form--button--fixed').style.marginTop = '0.6rem';
+            }
+        }
+
+        function changeMarginL(dq) {
+
+            if (dq.matches) {
+                document.body.querySelector('.search__bar__wrapper--header--fixed').style.marginTop = '1rem';
+                document.body.querySelector('.search__bar__form--fixed').style.marginTop = '0';
+                document.body.querySelector('.search__bar__form--button--fixed').style.marginTop = '.6rem';
+            }
+        }
+
+    } else if (distance <= 10) {
+        parent.className = 'search__bar';
+        wrapper.className = 'search__bar__wrapper';
+        header.className = 'search__bar__wrapper--header';
+        form.className = 'search__bar__form';
+        formContainer.className = 'search__bar__form--container';
+        formIcon.className = 'search__bar__form--icon';
+        artistQuery.className = 'search__bar__form--input';
+        searchBtn.className = 'search__bar__form--button';
+
     }
+}
 
 // Function to calculate how much percentage of the page has been scrolled down by the user
 
@@ -169,16 +253,16 @@ function calculateScrollHeight() {
     const windowHeight = window.innerHeight; /* Get the height of the browser window */
     const scrollTop = window.pageYOffset; /* How much user scrolled page vertically */
     const docHeight = getDocumentHeight(); /* Document height */
-    const heightDiff = docHeight - windowHeight;/* Difference between document height and browser height */
-    const percentageScrolled = Math.floor(scrollTop/heightDiff * 100); /* Percentage of page scrolled by the user */
+    const heightDiff = docHeight - windowHeight; /* Difference between document height and browser height */
+    const percentageScrolled = Math.floor(scrollTop / heightDiff * 100); /* Percentage of page scrolled by the user */
     return percentageScrolled;
 }
 
-function getDocumentHeight(){
+function getDocumentHeight() {
     const doc = document;
     return Math.max(doc.body.scrollHeight,
-                    doc.body.offsetHeight,
-                    doc.body.clientHeight);
+        doc.body.offsetHeight,
+        doc.body.clientHeight);
 }
 
 
